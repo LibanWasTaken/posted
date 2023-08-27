6bd6842cd9f2a229bceb8c539f79e55e130ece06
Liban MR <libanmesbah@gmail.com>
2023-08-27 11:28:56 +0600

    useContext for account instead of..that

--- src/pages/SettingsPage.js
+++ src/pages/SettingsPage.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
-import { AccPage } from "./accountPage";
+import { useUserContext } from "../context/UserContext";
+import { Spinner1 } from "../components/Spinner";
+
 import {
   getDatabase,
   ref,
@@ -11,21 +13,17 @@ import {
 } from "firebase/database";
 
 const SettingsPage = () => {
+  const { user: currentUser, loading } = useUserContext();
   const [userInfo, setUserInfo] = useState(null);
 
   // Setting User and Database
-  const values = AccPage();
   const db = getDatabase();
-  let currentUser;
-  if (values.logged) {
-    currentUser = values.user;
-  }
   useEffect(() => {
     if (currentUser) {
       const infoRef = ref(db, "users/" + currentUser.uid + "/info");
       onValue(infoRef, (snapshot) => {
         let data = snapshot.val();
-        setUserInfo(data);
+        s(data);
       });
     }
   }, [currentUser]);
@@ -67,60 +65,66 @@ const SettingsPage = () => {
   }
   return (
     <Wrapper>
-      {currentUser ? (
-        <div>
-          <button className="classicBtn" onClick={updateUserDetails}>
-            Submit
-          </button>
-          <div className="section recipient">
-            <h1>Who are you going to send tho? </h1>
-            <h4>tip on who to send, how etc</h4>
-            <h3>Mail 1, short msg 100 letter</h3>
-            <h3>Mail 2, short msg 100 letter</h3>
-            <button className="classicBtn">Edit</button>
-            <h2>More..</h2>
-            <h3>default - mail 3 - self </h3>
-            <h3>default - How long should delays - 1month</h3>
-            <h3>default - when - 1st day of month</h3>
-            <h3>default - - </h3>
-          </div>
-          <div className="section recipient">
-            <h1>Who are you?</h1>
-            <h2>how much do u want to reveal</h2>
-            <h3>First Name</h3>
-            <h3>Last Name</h3>
-            <h3>Middle Name</h3>
-            <button className="classicBtn">Edit</button>
-
-            <h3>Prefix</h3>
-            <h3>Suffix</h3>
-            <h3>Date of Birth</h3>
-            <h3>City of Birth</h3>
-            <h2>more..</h2>
-
-            <h3>State of Birth</h3>
-            <h3>Country of Birth</h3>
-            <h3>Gender</h3>
-            <h3>Phone Number</h3>
-            <h3>Email Address</h3>
-            <h2>More..</h2>
-
-            <h3>Home Address</h3>
-            <h3>Mailing Address</h3>
-            <h3>Occupation</h3>
-            <h3>Spouse-s</h3>
-            <h3>Children-s</h3>
-            <h3>Blood Type</h3>
-            <h2>MORE..</h2>
-
-            <h3>Medical Conditions</h3>
-            <h3>Medications</h3>
-            <h3>Social Media Profiles</h3>
-            <h3>Online Usernames</h3>
-          </div>
-        </div>
+      {loading ? (
+        <Spinner1 />
       ) : (
-        <h1>Log in</h1>
+        <div>
+          {currentUser ? (
+            <div>
+              <button className="classicBtn" onClick={updateUserDetails}>
+                Submit
+              </button>
+              <div className="section recipient">
+                <h1>Who are you going to send tho? </h1>
+                <h4>tip on who to send, how etc</h4>
+                <h3>Mail 1, short msg 100 letter</h3>
+                <h3>Mail 2, short msg 100 letter</h3>
+                <button className="classicBtn">Edit</button>
+                <h2>More..</h2>
+                <h3>default - mail 3 - self </h3>
+                <h3>default - How long should delays - 1month</h3>
+                <h3>default - when - 1st day of month</h3>
+                <h3>default - - </h3>
+              </div>
+              <div className="section recipient">
+                <h1>Who are you?</h1>
+                <h2>how much do u want to reveal</h2>
+                <h3>First Name</h3>
+                <h3>Last Name</h3>
+                <h3>Middle Name</h3>
+                <button className="classicBtn">Edit</button>
+
+                <h3>Prefix</h3>
+                <h3>Suffix</h3>
+                <h3>Date of Birth</h3>
+                <h3>City of Birth</h3>
+                <h2>more..</h2>
+
+                <h3>State of Birth</h3>
+                <h3>Country of Birth</h3>
+                <h3>Gender</h3>
+                <h3>Phone Number</h3>
+                <h3>Email Address</h3>
+                <h2>More..</h2>
+
+                <h3>Home Address</h3>
+                <h3>Mailing Address</h3>
+                <h3>Occupation</h3>
+                <h3>Spouse-s</h3>
+                <h3>Children-s</h3>
+                <h3>Blood Type</h3>
+                <h2>MORE..</h2>
+
+                <h3>Medical Conditions</h3>
+                <h3>Medications</h3>
+                <h3>Social Media Profiles</h3>
+                <h3>Online Usernames</h3>
+              </div>
+            </div>
+          ) : (
+            <h1>Log in</h1>
+          )}
+        </div>
       )}
     </Wrapper>
   );
